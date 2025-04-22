c956f0f9f7a83a26f13443493a273a95cfbdcaa1
Aurora Esmeralda <auroraesmeralda@protonmail.com>
2025-04-22 12:47:53 +1000

    Revert movement changes

--- Assets/Scripts/ControllableCharacter.cs
+++ Assets/Scripts/ControllableCharacter.cs
@@ -1,90 +1,54 @@
-using System;
 using UnityEngine;
 
 public class ControllableCharacter : MonoBehaviour
 {
-    private CharacterController charCtrl;
-    private Rigidbody childRb;
-    private MovementController ctrl;
+    public ControllableEntityParams Params;
     private Vector2 moveDir;
-    private Vector2 nonZeroMoveDir;
-    private Vector2 prevNonZeroMoveDir;
-    private Vector3 moveForce;
-    private bool accelerating;
+    private bool accelerating = false;
     private float velocity;
-    private float rotationCompletion = 0.0f;
-    private float rotationTime => 1.0f / ctrl.MovementParams.RotationSpeed;
+    private Vector3 lastMove;
+
+    private Rigidbody rb;
 
     public void SetInputAxes(Vector2 input)
     {
-        Vector2 rotatedInput = input.Rotate(ctrl.ForwardDirection.Angle()).normalized;
         accelerating = !input.Equals(Vector2.zero);
-        if (accelerating && !moveDir.Equals(rotatedInput))
-            // Move dir has changed; we should rotate
-            if (!rotatedInput.Equals(Vector2.zero))
-            {
-                prevNonZeroMoveDir = nonZeroMoveDir.Equals(rotatedInput) ? prevNonZeroMoveDir : nonZeroMoveDir;
-                nonZeroMoveDir = rotatedInput;
-                rotationCompletion = 0.0f;
-            }
-            moveDir = rotatedInput;
+        if (accelerating)
+            moveDir = input.normalized;
     }
-
-    public void SetController(MovementController ctrl) { this.ctrl = ctrl; }
 
     void Start()
     {
-        charCtrl = GetComponent<CharacterController>();
-        childRb = GetComponentInChildren<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Rotate cat to look in moving direction (VERY CRUDE)
+        if (!moveDir.Equals(Vector2.zero))
+            transform.rotation = Quaternion.LookRotation(moveDir.Rotate(-90).ToVector3XZ());
     }
 
     void FixedUpdate()
     {
         Vector3 movement = Vector3.zero;
-        Vector3 gravity = Vector3.down * 6.0f;
-        float velocityDelta = (accelerating ? ctrl.Acceleration : -ctrl.Deceleration) * Time.fixedDeltaTime;
-        velocity = Mathf.Clamp(velocity + velocityDelta, 0, ctrl.MaxVelocity);
+        float velocityDelta = (accelerating ? Params.Acceleration : -Params.Acceleration) * Time.fixedDeltaTime;
+        velocity = Mathf.Clamp(velocity + velocityDelta, 0, Params.MaxVelocity);
         Vector3 walk = moveDir.ToVector3XZ() * velocity;
-        movement += gravity;
         movement += walk;
-        Vector3 movementTimeScaled = movement * Time.fixedDeltaTime;
-        RaycastHit hit;
-        Vector3 avoidCollisions = childRb.SweepTest(movement.normalized, out hit, movementTimeScaled.magnitude)
-            ? movementTimeScaled.normalized * (hit.distance + 0.01f)
-            : movementTimeScaled;
-        charCtrl.Move(avoidCollisions);
-        moveForce = new Vector3(movementTimeScaled.x, 0.0f, movementTimeScaled.z);
-
-        // Rotation
-        Vector2 rotationVec = nonZeroMoveDir;
-        if (rotationCompletion < 1.0f)
-        {
-            float deltaCompletion = Time.fixedDeltaTime / rotationTime;
-            rotationCompletion += deltaCompletion;
-            float totalRotation = Vector2.SignedAngle(prevNonZeroMoveDir, nonZeroMoveDir);
-            float thisRotation = Mathf.Lerp(0, totalRotation, rotationCompletion);
-            rotationVec = prevNonZeroMoveDir.Rotate(thisRotation);
-        }
-        if (!rotationVec.Equals(Vector2.zero))
-            transform.rotation = Quaternion.LookRotation(rotationVec.Rotate(-90).ToVector3XZ());
+        lastMove = movement;
+        rb.MovePosition(rb.position + lastMove * Time.fixedDeltaTime);
     }
 
-    void OnControllerColliderHit(ControllerColliderHit hit)
+    void OnCollisionEnter(Collision collision)
     {
-        PushBlock(hit.gameObject, hit.rigidbody);
-    }
-
-    public void PushBlock(GameObject obj, Rigidbody rb)
-    {
-        if (obj.CompareTag("Pushable"))
+        if (collision.gameObject.CompareTag("Pushable"))
         {
-            Vector3 blockRelativePos = (rb.position - transform.position).normalized;
-            Vector3 moveDir = nonZeroMoveDir.ToVector3XZ();
-            float angle = Vector3.Angle(moveDir, blockRelativePos);
-            if (angle < 90.0f)
-            {
-                obj.GetComponent<FixRigidbody>().Move(moveForce);
-            }
+            Vector3 moveDir = lastMove.normalized;
+            Vector3 blockDir = rb.position - collision.rigidbody.position;
+            float angle = Vector3.Angle(moveDir, blockDir);
+            if (angle < 75.0f)
+                collision.rigidbody.MovePosition(collision.rigidbody.position + lastMove * (Time.fixedDeltaTime + 0.05f));
         }
     }
 }
--- Assets/Scripts/ControllableEntityParams.cs
+++ Assets/Scripts/ControllableEntityParams.cs
@@ -6,8 +6,8 @@ public class ControllableEntityParams : ScriptableObject
     public Vector2 ForwardDirection = Vector2.right;
     public float MaxVelocity = 1.0f;
     public float Acceleration = 1.0f;
-    public float Deceleration = 1.0f;
     public float RotationSpeed = 1.0f;
     public float MaxClimbHeight = 0.2f;
     public float MaxSlopeAngle = 45.0f;
+    public float GravityStrength = 5.0f;
 }
