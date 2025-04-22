f5b7fde85931dcd6829dc34cb2ad0397723bfb41
Aurora Esmeralda <auroraesmeralda@protonmail.com>
2025-04-23 00:18:05 +1000

    Add push down/up to buttons:

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,18 +1,36 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Button : MonoBehaviour
 {
     [SerializeField] private List<ToggleableEntity> toggleOnPress = new List<ToggleableEntity>();
+    [SerializeField] private float buttonMoveSpeed = 1.0f;
+    private GameObject buttonChild;
+    private float targetHeight;
+
+    void Start()
+    {
+        buttonChild = transform.GetChild(0).gameObject;
+    }
+
+    void FixedUpdate()
+    {
+        float height = buttonChild.transform.localPosition.y;
+        float delta = Mathf.MoveTowards(height, targetHeight, buttonMoveSpeed * Time.fixedDeltaTime);
+        buttonChild.transform.localPosition = Vector3.up * delta;
+    }
 
     void OnTriggerEnter(Collider other)
     {
         TriggerCollide(other);
+        targetHeight = -0.02f;
     }
 
     void OnTriggerExit(Collider other)
     {
         TriggerCollide(other);
+        targetHeight = 0.0f;
     }
 
     private void TriggerCollide(Collider other)
