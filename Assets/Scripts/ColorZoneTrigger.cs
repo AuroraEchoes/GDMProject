1eb2daafb5b1815584924c6f55fb3d816a7d02d5
GGHUANG-BOOM <gordon.g.huang@student.uts.edu.au>
2025-04-23 15:16:55 +1000

    update

--- Assets/Scripts/ColorZoneTrigger.cs
+++ Assets/Scripts/ColorZoneTrigger.cs
@@ -8,25 +8,25 @@ public class ColorZoneTrigger : MonoBehaviour
     void Start()
     {
         puzzleManager = FindObjectOfType<ColorPuzzle>();
-        Debug.Log($"{ZoneName} expects: {correctBox.name}");
+        
     }
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{other.gameObject.name} entered {ZoneName} zone");
+        
         if (other.gameObject == correctBox)
         {
-            Debug.Log($"{ZoneName}: Correct box entered.");
+            
             puzzleManager.SetBoxState(ZoneName, true);
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        Debug.Log($"{other.gameObject.name} exited {ZoneName} zone");
+       
         if (other.gameObject == correctBox)
         {
-            Debug.Log($"{ZoneName}: Correct box exited.");
+          
             puzzleManager.SetBoxState(ZoneName, false);
         }
     }
