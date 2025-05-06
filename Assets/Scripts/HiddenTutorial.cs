b9f5c4715c7473553e8d53142b4a0f6a75d92901
PurpleflamingClouds <alexander.maturan@gmail.com>
2025-05-06 22:25:42 +1000

    Included all next levels in every level and fixed minor bugs

--- Assets/Scripts/HiddenTutorial.cs
+++ Assets/Scripts/HiddenTutorial.cs
@@ -5,10 +5,18 @@ public class HiddenTutorial : MonoBehaviour
 
     public GameObject boxPrefab;
 
+    bool triggeronce = true;
+
     void OnTriggerExit(Collider other)
     {
-        Debug.Log("Triggered");
-        Instantiate(boxPrefab, new Vector3(-12.20428f, 5.43f, -8.23f), Quaternion.identity);
+        if (triggeronce)
+        {
+            Debug.Log("Triggered");
+            Instantiate(boxPrefab, new Vector3(-12.20428f, 5.43f, -8.23f), Quaternion.identity);
+            triggeronce = false;
+        }
+
+
     }
 
 
