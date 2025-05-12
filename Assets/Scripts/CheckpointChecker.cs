1eeb7eb4f3ae07ed85ea05b16f7ba7ee521fb541
Aurora Esmeralda <auroraesmeralda@protonmail.com>
2025-05-12 11:08:06 +1000

    Change flag colours on activation

--- Assets/Scripts/CheckpointChecker.cs
+++ Assets/Scripts/CheckpointChecker.cs
@@ -1,18 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class CheckpointChecker : MonoBehaviour
 {
-
+    private Renderer rend;
     public static Vector3 LightCatCheckpoint { get; private set; }
     public static Vector3 ShadowCatCheckpoint { get; private set; }
     public static bool HasCheckpoint { get; private set; }
     public static bool HasCheckpointTwo { get; private set; }
-
+    public Color DeactivatedColor;
+    public Color ActivatedColor;
 
     private bool triggerLightOnce = false;
     private bool triggerShadowOnce = false;
 
-
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = DeactivatedColor;
+    }
 
     private void OnTriggerExit(Collider other)
     {
@@ -21,6 +27,7 @@ public class CheckpointChecker : MonoBehaviour
             LightCatCheckpoint = other.transform.position;
             triggerLightOnce = true;
             HasCheckpoint = true;
+            StartCoroutine(TweenFlagColor());
             Debug.Log("Light kitty is at " + LightCatCheckpoint);
         }
         else if (other.CompareTag("Shadow") && !triggerShadowOnce)
@@ -28,19 +35,33 @@ public class CheckpointChecker : MonoBehaviour
             ShadowCatCheckpoint = other.transform.position;
             triggerShadowOnce = true;
             HasCheckpointTwo = true;
+            StartCoroutine(TweenFlagColor());
             Debug.Log("Shadow Kitty is at " + ShadowCatCheckpoint);
         }
-
     }
 
-
-
-
+    private IEnumerator TweenFlagColor()
+    {
+        float t = 0.0f;
+        while (t < 0.2f)
+        {
+            t += Time.deltaTime;
+            rend.material.color = Color.Lerp(DeactivatedColor, ActivatedColor, t);
+            yield return null;
+        }
+        rend.material.color = ActivatedColor;
+    }
 
     public static void ResetCheckpoints()
     {
         LightCatCheckpoint = Vector3.zero;
         ShadowCatCheckpoint = Vector3.zero;
         HasCheckpoint = false;
+    }
+
+    public enum Cat
+    {
+        Light,
+        Shadow
     }
 }
